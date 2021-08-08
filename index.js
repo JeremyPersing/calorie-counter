ddfe60aa74c66956baf9d49ca60b994af778f82c
Jeremy Persing <34422870+JeremyPersing@users.noreply.github.com>
2021-08-07 18:52:19 -0700

    deployment

--- index.js
+++ index.js
@@ -44,11 +44,12 @@ cron.schedule("0 0 * * 0-6", async () => {
     console.log("error in deleting conumed meals for the day", error);
   }
 });
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "/client/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
